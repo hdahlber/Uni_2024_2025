c18ab23c72ae5a3fe02efc0074f03d9326f2433c
hdahlber <hdahlber@abo.fi>
2024-12-16 11:57:14 +0200

    webtech: serializer added for transactions

--- Web_Tech/Backend/transactions/models.py
+++ Web_Tech/Backend/transactions/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from users.models import User
+
+
 # Create your models here.
 class Transaction(models.Model):
     transaction_id = models.CharField(max_length=255, unique=True)
     date = models.DateTimeField(auto_now_add=True)
     buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="transactions")
+    
 
 def __str__(self):
     return self.transaction_id
